6884fb7c519f28e77e6e2b56963ae3fc3aff0035
HassanAlwakeel1 <hassanalwakeel911@gmail.com>
2025-08-27 02:11:32 +0300

    add CinemaGo project files

---
+++ src/main/java/com/CinemaGo/CinemaGoApplication.java
@@ -0,0 +1,13 @@
+package com.CinemaGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CinemaGo/CinemaGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CinemaGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
